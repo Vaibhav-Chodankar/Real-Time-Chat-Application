f054d37fcb1e3fced1c73ed6d800adc8901b305f
Vaibhav-Chodankar <vaibhavac03@gmail.com>
2024-09-22 10:26:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ const server = app.listen(process.env.PORT, () => {
 
 const io = socket(server, {
     cors: {
-        origin: 'https://real-time-chat-application-ashen.vercel.app/',
+        origin: 'https://real-time-chat-application-ashen.vercel.app',
         credentials: true,
     },
 });
